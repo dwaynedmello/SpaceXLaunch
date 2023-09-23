4ffe3788fd98da9cb354a33721ef966629071e5f
dwaynedmello <98520328+dwaynedmello@users.noreply.github.com>
2023-09-23 17:01:05 +0200

    Cypress test code and /launchList creation
    to test the code

--- src/LaunchList.tsx
+++ src/LaunchList.tsx
@@ -77,164 +77,155 @@ function LaunchList() {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [reachedBottom, setReachedBottom] = useState<boolean>(false); // Track if bottom is reached
     const [isLastPage, setIsLastPage] = useState<boolean>(false); // Track if it's the last page
-
+  
     const bottomOfPageRef = useRef<HTMLDivElement | null>(null);
-
+  
     const loadLaunches = useCallback(async () => {
-
-        setIsLoading(true);
-        try {
-            const data = await fetchSpaceXLaunches(page, pageSize);
-            // If it's not the first page, append new data to existing data
-            if (data.length === 0) {
-                setIsLastPage(true);
-            }
-            setLaunches((prevLaunches) => [...prevLaunches, ...data]);
-        } catch (error) {
-            console.error('Error loading SpaceX launches', error);
-        } finally {
-            setIsLoading(false);
-            setReachedBottom(false)
+      setIsLoading(true);
+      try {
+        const data = await fetchSpaceXLaunches(page, pageSize);
+        if (data.length === 0) {
+          setIsLastPage(true);
         }
+        setLaunches((prevLaunches) => [...prevLaunches, ...data]);
+      } catch (error) {
+        console.error('Error loading SpaceX launches', error);
+      } finally {
+        setIsLoading(false);
+        setReachedBottom(false);
+      }
     }, [page]);
-
+  
     useEffect(() => {
-        
-        // Reset page when search or filter criteria change
-        setPage(1);
-        setIsLastPage(false); // Reset the last page flag
-        loadLaunches();
+      setPage(1);
+      setIsLastPage(false);
+      loadLaunches();
     }, [searchQuery, filterCriteria]);
-
+  
     useEffect(() => {
-        console.log(isLoading, reachedBottom, isLastPage);
-        console.log('Page changed:', page);
-        loadLaunches();
+      loadLaunches();
     }, [loadLaunches]);
-
-    // IntersectionObserver callback function
+  
     const handleIntersection = (entries: IntersectionObserverEntry[]) => {
-        if (entries[0].isIntersecting && !isLoading && !reachedBottom && !isLastPage) {
-            // When the last element comes into view, set the flag to prevent multiple updates
-            setReachedBottom(true);
-            setPage((prevPage) => prevPage + 1);
-        }
+      if (entries[0].isIntersecting && !isLoading && !reachedBottom && !isLastPage) {
+        setReachedBottom(true);
+        setPage((prevPage) => prevPage + 1);
+      }
     };
-
+  
     useEffect(() => {
-        // Create an IntersectionObserver to observe the bottomOfPageRef
-        const observer = new IntersectionObserver(handleIntersection, {
-            root: null,
-            rootMargin: '0px',
-            threshold: 1.0, // Trigger when the element is fully in view
-        });
-
+      const observer = new IntersectionObserver(handleIntersection, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0,
+      });
+  
+      if (bottomOfPageRef.current) {
+        observer.observe(bottomOfPageRef.current);
+      }
+  
+      return () => {
         if (bottomOfPageRef.current) {
-            observer.observe(bottomOfPageRef.current);
+          observer.unobserve(bottomOfPageRef.current);
         }
-
-        // Clean up the observer when the component unmounts
-        return () => {
-            if (bottomOfPageRef.current) {
-                observer.unobserve(bottomOfPageRef.current);
-            }
-        };
+      };
     }, [isLoading, isLastPage]);
-
-    // Filter launches based on filterCriteria and searchQuery
+  
     const filteredLaunches = launches.filter((launch) => {
-        const lowerCaseQuery = searchQuery.toLowerCase();
-        const lowerCaseName = launch.name.toLowerCase();
-
-        // Check if the launch name and details contain the search query
-        const matchesQuery =
-            lowerCaseName.includes(lowerCaseQuery) ||
-            (launch.details &&
-                launch.details.toLowerCase().includes(lowerCaseQuery));
-
-        // Filter based on the filterCriteria
-        if (filterCriteria === 'all') {
-            return matchesQuery;
-        } else if (filterCriteria === 'successful') {
-            return matchesQuery && launch.success;
-        } else if (filterCriteria === 'failed') {
-            return matchesQuery && !launch.success;
-        }
-
+      const lowerCaseQuery = searchQuery.toLowerCase();
+      const lowerCaseName = launch.name.toLowerCase();
+  
+      const matchesQuery =
+        lowerCaseName.includes(lowerCaseQuery) ||
+        (launch.details &&
+          launch.details.toLowerCase().includes(lowerCaseQuery));
+  
+      if (filterCriteria === 'all') {
         return matchesQuery;
+      } else if (filterCriteria === 'successful') {
+        return matchesQuery && launch.success;
+      } else if (filterCriteria === 'failed') {
+        return matchesQuery && !launch.success;
+      }
+  
+      return matchesQuery;
     });
-
+  
     return (
-        <Container>
-            <Typography variant="h4" align="center" gutterBottom>
-                SpaceX Launches
-            </Typography>
-            <Box display="flex" alignItems="center" justifyContent="space-between">
-                {/* Search input */}
-                <TextField
-                    label="Search"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
+      <Container>
+        <Typography variant="h4" align="center" gutterBottom>
+          SpaceX Launches
+        </Typography>
+        <Box display="flex" alignItems="center" justifyContent="space-between">
+          <TextField
+            label="Search"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="search-input" // Added className for testing
+          />
+          <FormControl variant="outlined" style={{ minWidth: '200px' }}>
+            <InputLabel>Filter</InputLabel>
+            <Select
+              value={filterCriteria}
+              onChange={(e) => setFilterCriteria(e.target.value as string)}
+              label="Filter"
+              className="filter-select" 
+            >
+              <MenuItem value="all">All Launches</MenuItem>
+              <MenuItem value="successful">Successful Launches</MenuItem>
+              <MenuItem value="failed">Failed Launches</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        <Grid container spacing={3}>
+          {filteredLaunches.map((launch, index) => (
+            <Grid item xs={12} sm={6} md={4} key={`${launch.id}-${index}`}>
+              <Card className="launch-card"> 
+                <CardHeader title={launch.name} />
+                <CardMedia
+                  component="img"
+                  alt={launch.name}
+                  height="200"
+                  image={launch.links.patch.small}
+                  style={{ objectFit: 'cover' }}
                 />
-                {/* Filter dropdown */}
-                <FormControl variant="outlined" style={{ minWidth: '200px' }}>
-                    <InputLabel>Filter</InputLabel>
-                    <Select
-                        value={filterCriteria}
-                        onChange={(e) => setFilterCriteria(e.target.value as string)}
-                        label="Filter"
-                    >
-                        <MenuItem value="all">All Launches</MenuItem>
-                        <MenuItem value="successful">Successful Launches</MenuItem>
-                        <MenuItem value="failed">Failed Launches</MenuItem>
-                    </Select>
-                </FormControl>
-            </Box>
-            <Grid container spacing={3}>
-                {filteredLaunches.map((launch, index) => (
-                    <Grid item xs={12} sm={6} md={4} key={`${launch.id}-${index}`}>
-                        <Card>
-                            <CardHeader title={launch.name} />
-                            <CardMedia
-                                component="img"
-                                alt={launch.name}
-                                height="200"
-                                image={launch.links.patch.small}
-                                style={{ objectFit: 'cover' }}
-                            />
-                            <CardContent>
-                                <Typography variant="body2" color="textSecondary">
-                                    Details: {truncateText(launch.details, 100)}
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary">
-                                    Launch Date: {new Date(launch.date_utc).toLocaleDateString()}
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary">
-                                    Status: {launch.success ? 'Successful' : 'Failed'}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
+                <CardContent>
+                  <Typography variant="body2" color="textSecondary">
+                    Details: {truncateText(launch.details, 100)}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    Launch Date: {new Date(launch.date_utc).toLocaleDateString()}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    Status: {launch.success ? 'Successful' : 'Failed'}
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
-
-            <div ref={bottomOfPageRef}>
-                {isLoading && (
-                    <Box display="flex" justifyContent="center" my={3}>
-                        <CircularProgress />
-                    </Box>
-                )}
-                {isLastPage && (
-                    <Typography variant="body2" color="textSecondary" style={{ textAlign: 'center', margin: '16px 0' }}>
-                        End of launches
-                    </Typography>
-                )}
-            </div>
-        </Container>
+          ))}
+        </Grid>
+  
+        <div ref={bottomOfPageRef}>
+          {isLoading && (
+            <Box display="flex" justifyContent="center" my={3}>
+              <CircularProgress />
+            </Box>
+          )}
+          {isLastPage && (
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              style={{ textAlign: 'center', margin: '16px 0' }}
+            >
+              End of launches
+            </Typography>
+          )}
+        </div>
+      </Container>
     );
-}
-
-export default LaunchList;
+  }
+  
+  export default LaunchList;
